2ffb5144a12cf0b25ad2e51655cb1363d45a7662
syeda-khadeeja <syedakhadeeja49@gmail.com>
2021-08-04 21:51:35 +0500

    NETFLIX WEBSITE

---
+++ signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign In</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+</head>
+<body>
+<br><br>
+	<h3>Sign In Page</h3><br>
+	
+	<form action="" method="post">
+		<input type="text" name="email" required>
+		<input type="Password" name="password" required>
+		<input type="submit" name="submit" required>
+	</form>
+
+	<?php 
+	
+	
+	?>
+
+</body>
+</html>
